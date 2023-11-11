781a96edab1458136aa0516724a5e5b2f6750142
davidSimek <david.simek@infis.cz>
2023-11-11 20:54:31 +0100

    yea yea

--- build.sh
+++ build.sh
@@ -1,24 +1,55 @@
 #!/bin/bash
 
+# -------- 
+# LOGGING
+# --------
+
+REDF="\e[31m"
+GREENF="\e[32m"
+BLUEF="\e[34m"
+ENDCOLOR="\e[0m"
+
+info() {
+    echo -e "${BLUEF}[INFO]${ENDCOLOR}$1"
+}
+success() {
+    echo -e "${GREENF}[SUCCESS]${ENDCOLOR}$1"
+}
+error() {
+    echo -e "${REDF}[ERROR]${ENDCOLOR}$1"
+}
+
+# ---------
+# BUILDING
+# ---------
+
 # WINDOWS
 build_windows() {
-    echo "building for Windows"
-    if [[ $1 = "g" ]]; then
-        echo "building with debug flags"
+    info "building for Windows"
+    if [[ $1 = "-g" ]]; then
+        info "building with debug flags"
     fi
+    error "unimplemented for now"
 }
 
 # GNU/LINUX
 build_linux() {
-    echo "building for Windows"
-    if [[ $1 = "g" ]]; then
-        echo "building with debug flags"
+    info "building for GNU/Linux"
+    if [[ $1 = "-g" ]]; then
+        info "building with debug flags"
+        error "unimplemented for now"
     fi
+    gcc -o out/http src/main.c
 }
 
-if [ "$1" = "-w" ]; then
-    windows_function
-else
-    other_function
-fi
+# -----
+# MAIN
+# -----
 
+if [ "$1" = "-w" ]; then
+    build_windows $2
+elif [[ "$1" = "-l" ]]; then
+    build_linux $2
+else
+    error "usage: build [-w(Windows)|-l(GNU/Linux] -g(?)"
+fi
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("hello this should be server");
+    return 0;
+}
