784b9fa4f8a2b4c2330d4d65c4c1fa9dd1992940
davidSimek <david.simek@infis.cz>
2023-11-12 15:26:09 +0100

    updated readme

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include "httpLibrary.h"
+
+#include <stdio.h>
 #include <string.h>
 
 #define REQUEST_MAX_LENGTH 1000
@@ -9,15 +10,15 @@ int main(int argc, char *argv[])
     HttpConfig config;
     setPort(&config, 8080);
     setupServer(&config);
-    char* request = (char*)malloc(REQUEST_MAX_LENGTH + 1);
+    char* requestBuffer = (char*)malloc(REQUEST_MAX_LENGTH + 1);
 
-    size_t requestLength = getRequest(&config, request, REQUEST_MAX_LENGTH);
-    printf("request is :\n%s\n", request);
+    size_t requestLength = getRequest(&config, requestBuffer, REQUEST_MAX_LENGTH);
+    printf("request is :\n%s\n", requestBuffer);
 
     char* response = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello, World!";
     sendResponse(&config, response, strlen(response));
     cleanupServer(&config);
 
-    free(request);
+    free(requestBuffer);
     return 0;
 }
